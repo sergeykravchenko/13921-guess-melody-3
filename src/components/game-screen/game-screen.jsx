b02fc474c6f5c452956edef76f910984a8407114
Sergey Kravchenko <kravchenko.me@gmail.com>
2020-02-24 17:39:17 +0300

    прошелся по демке

--- src/components/game-screen/game-screen.jsx
+++ src/components/game-screen/game-screen.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {connect} from "react-redux";
 import {GameType} from "../../const.js";
+import Mistakes from "../mistakes/mistakes.jsx";
 
 const GameScreen = (props) => {
   const {
     type,
     children,
+    mistakes,
   } = props;
 
   return (
@@ -21,11 +24,9 @@ const GameScreen = (props) => {
             style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
         </svg>
 
-        <div className="game__mistakes">
-          <div className="wrong" />
-          <div className="wrong" />
-          <div className="wrong" />
-        </div>
+        <Mistakes
+          count={mistakes}
+        />
       </header>
 
       {children}
@@ -39,6 +40,12 @@ GameScreen.propTypes = {
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
   ]).isRequired,
+  mistakes: PropTypes.number.isRequired,
 };
 
-export default GameScreen;
+const mapStateToProps = (state) => ({
+  mistakes: state.mistakes,
+});
+
+export {GameScreen};
+export default connect(mapStateToProps)(GameScreen);
